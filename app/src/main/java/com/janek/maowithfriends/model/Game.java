5779303e8201eeaffa51818902fec977ede408d6
Janek Brandt <brandt.janek@gmail.com>
2017-06-19 22:35:47 -0700

    add turn tracker to game object

--- app/src/main/java/com/janek/maowithfriends/model/Game.java
+++ app/src/main/java/com/janek/maowithfriends/model/Game.java
@@ -2,7 +2,10 @@ package com.janek.maowithfriends.model;
 
 import org.parceler.Parcel;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Parcel
@@ -10,14 +13,15 @@ public class Game {
     String gameId;
     String currentPlayer;
     Map<String, Player> players = new HashMap<>();
-    Deck deck;
+    Map<String, String> nextPlayerTurn = new HashMap<>();
+    List<Card> deck;
 
     public Game() {}
 
     public Game(String gameId, String currentPlayer) {
         this.gameId = gameId;
         this.currentPlayer = currentPlayer;
-        this.deck = Deck.createNewDeck();
+        this.deck = Game.createNewDeck();
     }
 
     public String getGameId() {
@@ -32,8 +36,20 @@ public class Game {
         return players;
     }
 
-    public Deck getDeck() {
+    public List<Card> getDeck() {
         return deck;
+    }
+
+    public static List<Card> createNewDeck() {
+        List<Card> newDeck = new ArrayList<>();
+        for (CardValue value : CardValue.values()) {
+            for (Suit suit : Suit.values()) {
+                Card newCard = new Card(value, suit);
+                newDeck.add(newCard);
+            }
+        }
+        Collections.shuffle(newDeck);
+        return newDeck;
     }
 
     public void addPlayer(Player player) {
@@ -41,10 +57,28 @@ public class Game {
     }
 
     public void dealCards() {
+        setTurns();
         for (Player player: players.values()) {
             for (int i = 0; i < 6; i ++) {
-                player.drawCard(deck.getDeck().remove(0));
+                player.drawCard(deck.remove(0));
             }
         }
+    }
+
+    public void nextTurn() {
+        currentPlayer = nextPlayerTurn.get(currentPlayer);
+    }
+
+    private void setTurns() {
+        Map<String, String> nextTurn = new HashMap<>();
+        List<String> keys = new ArrayList<>(getPlayers().keySet());
+        for (int i = 0; i < keys.size(); i++) {
+            if (i == keys.size() - 1) {
+                nextTurn.put(keys.get(i), keys.get(0));
+            } else {
+                nextTurn.put(keys.get(i), keys.get(i+1));
+            }
+        }
+        nextPlayerTurn = nextTurn;
     }
 }
--- app/src/main/java/com/janek/maowithfriends/ui/GameActivity.java
+++ app/src/main/java/com/janek/maowithfriends/ui/GameActivity.java
@@ -5,24 +5,37 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.janek.maowithfriends.Constants;
 import com.janek.maowithfriends.R;
 import com.janek.maowithfriends.adapter.PlayerHandAdapter;
 import com.janek.maowithfriends.model.Game;
 
 import org.parceler.Parcels;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class GameActivity extends AppCompatActivity {
     @BindView(R.id.currentTurnPlayer) TextView currentTurnPlayer;
     @BindView(R.id.playerHandRecycleView) RecyclerView playerHandRecyclerView;
+    @BindView(R.id.nextTurnBtn) Button nextTurnBtn;
 
     PlayerHandAdapter playerHandAdapter;
 
@@ -45,9 +58,29 @@ public class GameActivity extends AppCompatActivity {
         playerHandRecyclerView.setAdapter(playerHandAdapter);
         playerHandRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
         playerHandRecyclerView.setHasFixedSize(true);
+
+        setGameState(newGame);
+
+        rootRef.child(Constants.FIREBASE_GAME_REF).child(newGame.getGameId()).addValueEventListener(new ValueEventListener() {
+            @Override public void onDataChange(DataSnapshot dataSnapshot) {
+                Game game = dataSnapshot.getValue(Game.class);
+                setGameState(game);
+            }
+
+            @Override public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+
     }
 
     private void setGameState(Game game) {
+        currentTurnPlayer.setText(game.getPlayers().get(game.getCurrentPlayer()).getName());
+
+
+        nextTurnBtn.setOnClickListener(view -> {
+            game.nextTurn();
+            rootRef.child(Constants.FIREBASE_GAME_REF).child(game.getGameId()).setValue(game);
+        });
 
     }
 }
