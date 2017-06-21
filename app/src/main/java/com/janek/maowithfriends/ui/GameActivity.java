f94364566e6a27a3a528ad896bb4106f571de5dd
Janek Brandt <brandt.janek@gmail.com>
2017-06-20 17:10:41 -0700

    add player icon and turn indicator to recyclerview at top of game activity

--- app/src/main/java/com/janek/maowithfriends/ui/GameActivity.java
+++ app/src/main/java/com/janek/maowithfriends/ui/GameActivity.java
@@ -7,10 +7,17 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -22,14 +29,20 @@ import com.google.firebase.database.ValueEventListener;
 import com.janek.maowithfriends.Constants;
 import com.janek.maowithfriends.R;
 import com.janek.maowithfriends.adapter.FirebasePlayerHandAdapter;
+import com.janek.maowithfriends.adapter.FirebasePlayerTurnViewHolder;
 import com.janek.maowithfriends.model.Card;
 import com.janek.maowithfriends.model.Game;
+import com.janek.maowithfriends.model.Player;
 
 import org.parceler.Parcels;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import io.reactivex.subjects.ReplaySubject;
 
 public class GameActivity extends AppCompatActivity {
     @BindView(R.id.currentTurnPlayer) TextView currentTurnPlayer;
@@ -39,8 +52,10 @@ public class GameActivity extends AppCompatActivity {
     @BindView(R.id.discardCardValue) TextView discardCardValue;
     @BindView(R.id.cardsLeft) TextView cardsLeft;
     @BindView(R.id.cardsDiscarded) TextView cardsDiscarded;
+    @BindView(R.id.playersLayout) RecyclerView playersLayout;
 
     private FirebasePlayerHandAdapter firebasePlayerHandAdapter;
+    private FirebaseRecyclerAdapter firebaseListAdapter;
 
     private FirebaseAuth mAuth;
     private FirebaseUser currentUser;
@@ -48,30 +63,45 @@ public class GameActivity extends AppCompatActivity {
     private DatabaseReference rootRef;
     private Game currentGame;
 
+    private ReplaySubject gameSubject = ReplaySubject.create();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
         ButterKnife.bind(this);
-        Game newGame = Parcels.unwrap(getIntent().getParcelableExtra("game"));
+        currentGame = Parcels.unwrap(getIntent().getParcelableExtra("game"));
 
         mAuth = FirebaseAuth.getInstance();
         currentUser = mAuth.getCurrentUser();
         uid = currentUser.getUid();
         rootRef = FirebaseDatabase.getInstance().getReference();
-        Query playerHandRef = rootRef.child(String.format(Constants.FIREBASE_PLAYER_HAND_REF, newGame.getGameId(), uid));
+        Query playerHandRef = rootRef.child(String.format(Constants.FIREBASE_PLAYER_HAND_REF, currentGame.getGameId(), uid));
+        DatabaseReference playerRef = rootRef.child(Constants.FIREBASE_GAME_REF).child(currentGame.getGameId()).child("players");
+
+        firebaseListAdapter = new FirebaseRecyclerAdapter<Player, FirebasePlayerTurnViewHolder>(Player.class, R.layout.player_turn_indicator,FirebasePlayerTurnViewHolder.class, playerRef) {
+            @Override
+            protected void populateViewHolder(FirebasePlayerTurnViewHolder viewHolder, Player model, int position) {
+                gameSubject.subscribe(game -> viewHolder.bindPlayer(model, (Game) game));
+            }
+        };
+        playersLayout.setAdapter(firebaseListAdapter);
+        playersLayout.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        playersLayout.setHasFixedSize(true);
 
         firebasePlayerHandAdapter = new FirebasePlayerHandAdapter(playerHandRef, this);
         playerHandRecyclerView.setAdapter(firebasePlayerHandAdapter);
         playerHandRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
         playerHandRecyclerView.setHasFixedSize(true);
 
-        setGameState(newGame);
+        setGameState();
 
-        rootRef.child(Constants.FIREBASE_GAME_REF).child(newGame.getGameId()).addValueEventListener(new ValueEventListener() {
+        rootRef.child(Constants.FIREBASE_GAME_REF).child(currentGame.getGameId()).addValueEventListener(new ValueEventListener() {
             @Override public void onDataChange(DataSnapshot dataSnapshot) {
                 Game game = dataSnapshot.getValue(Game.class);
-                setGameState(game);
+                currentGame = game;
+                gameSubject.onNext(currentGame);
+                setGameState();
             }
             @Override public void onCancelled(DatabaseError databaseError) {}
         });
@@ -98,8 +128,7 @@ public class GameActivity extends AppCompatActivity {
         }
     }
 
-    private void setGameState(Game game) {
-        currentGame = game;
+    private void setGameState() {
         updateTurnIndicator();
         updateDiscardCard(currentGame.topDiscardCard());
         cardsLeft.setText(String.format("Cards Left: %d", currentGame.getDeck().size()));
